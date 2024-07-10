551841959f0b1ad16359df82b3a87c5f6365bff4
subahan ali <subahanislam523@gmail.com>
2024-07-10 14:20:26 -0600

    home menu

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import ErrorPage from "./ErrorPage";
+import Home from "../pages/Home";
+import Login from "../pages/Authentication/Login";
+import Registration from "../pages/Authentication/Registration";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+        {
+            index:true,
+            element:<Home></Home>
+        },
+        {
+            path:'/login',
+            element:<Login></Login>
+        },
+        {
+            path:'/registration',
+            element:<Registration></Registration>
+        }
+    ]
+    
   },
 ]);
 
