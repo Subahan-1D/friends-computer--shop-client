1fd939af0eab0653922e12372e0b9d520b4b7c3b
subahan ali <subahanislam523@gmail.com>
2024-07-10 13:17:46 -0600

    project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRXYH9c1owEdfkbgG1SMOeWrNgRNoEW6Y",
+  authDomain: "assignment-11-b49bc.firebaseapp.com",
+  projectId: "assignment-11-b49bc",
+  storageBucket: "assignment-11-b49bc.appspot.com",
+  messagingSenderId: "530780318853",
+  appId: "1:530780318853:web:04ee225f22bff1d57b0dec",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      {/* Navbar */}
+<h1> main </h1>
+      {/* Outlet */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Main;
